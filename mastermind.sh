4d6c45ccfbe5331f953ced7ee1e65b96c6de5b8a
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2022-06-11 16:34:54 +0200

    feat: added black response

--- mastermind.sh
+++ mastermind.sh
@@ -8,16 +8,20 @@ printCompareResult(){
     local guess="$1"
     local code="$2"
     echo "$guess is not equal to $code"
+
+    for (( i=0; i<${#guess}; i++)) 
+    do
+        if [ ${code:$i:1} -eq ${guess:$i:1} ] 
+        then
+            echo "B"
+        fi
+    done
+    
 }
 
 run() {
     local code=$(generateCode)
     echo $code
-
-    for c in "$code"
-    do
-        echo $c
-    done
    
     for round in {1..12}
     do
